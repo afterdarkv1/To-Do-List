d107ba48d6cfb08e71eb15839f7722f2c82368ec
Luis <bamcuent312@gmail.com>
2023-08-24 23:35:26 +0200

    ad to finish clean code

--- src/index.js
+++ src/index.js
@@ -6,23 +6,22 @@ const $template = d.getElementById("crud-template").content;
 const $fragment = d.createDocumentFragment();
 
 const getAll = async () => {
-  
   try {
     let res = await axios.get("http://localhost:3000/task");
     let json = await res.data;
 
-    json.forEach(el => {
+    json.forEach((el) => {
       const $clone = d.importNode($template, true);
       const $taskRow = $clone.querySelector("tr");
-
 
       $taskRow.dataset.id = el.id;
       $clone.querySelector(".name").textContent = el.nameData;
       $clone.querySelector(".description").textContent = el.descriptionData;
-      $clone.querySelector(".task-done").checked = el.taskDone; 
+      $clone.querySelector(".task-done").checked = el.taskDone;
       $clone.querySelector(".edit").dataset.id = el.id;
       $clone.querySelector(".edit").dataset.nameData = el.nameData;
-      $clone.querySelector(".edit").dataset.descriptionData = el.descriptionData;
+      $clone.querySelector(".edit").dataset.descriptionData =
+        el.descriptionData;
       $clone.querySelector(".delete").dataset.id = el.id;
 
       $fragment.appendChild($clone);
@@ -31,21 +30,26 @@ const getAll = async () => {
     $table.querySelector("tbody").appendChild($fragment);
   } catch (err) {
     let message = err.statusText || "Ocurrió un error";
-    $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+    $table.insertAdjacentHTML(
+      "afterend",
+      `<p><b>Error ${err.status}: ${message}</b></p>`
+    );
   }
-}
-
+};
 
 d.addEventListener("DOMContentLoaded", () => {
-  getAll(); 
+  getAll();
+
   const $search = d.getElementById("search");
 
   $search.addEventListener("input", () => {
     const searchTerm = $search.value.toLowerCase();
     const $taskRows = $table.querySelectorAll("tr");
 
-    $taskRows.forEach($taskRow => {
-      const taskName = $taskRow.querySelector(".name").textContent.toLowerCase();
+    $taskRows.forEach(($taskRow) => {
+      const taskName = $taskRow
+        .querySelector(".name")
+        .textContent.toLowerCase();
       if (taskName.includes(searchTerm)) {
         $taskRow.style.display = "table-row";
       } else {
@@ -55,24 +59,23 @@ d.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
-d.addEventListener("submit", async e => {
+d.addEventListener("submit", async (e) => {
   if (e.target === $form) {
     e.preventDefault();
 
     if (!e.target.id.value) {
-      // Create - POST
+      
       try {
         let options = {
           method: "POST",
           headers: {
-            "Content-type": "application/json; charset=utf-8"
+            "Content-type": "application/json; charset=utf-8",
           },
           data: JSON.stringify({
             nameData: e.target.nameData.value,
             descriptionData: e.target.descriptionData.value,
-            taskDone: false 
-          })
+            taskDone: false,
+          }),
         };
         let res = await axios("http://localhost:3000/task", options);
         let json = await res.data;
@@ -80,36 +83,44 @@ d.addEventListener("submit", async e => {
         location.reload();
       } catch (err) {
         let message = err.statusText || "Ocurrió un error";
-        $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+        $form.insertAdjacentHTML(
+          "afterend",
+          `<p><b>Error ${err.status}: ${message}</b></p>`
+        );
       }
     } else {
-      // Update - PUT
 
       try {
         let options = {
           method: "PUT",
           headers: {
-            "Content-type": "application/json; charset=utf-8"
+            "Content-type": "application/json; charset=utf-8",
           },
           data: JSON.stringify({
             nameData: e.target.nameData.value,
             descriptionData: e.target.descriptionData.value,
-            taskDone: false 
-          })
+            taskDone: false,
+          }),
         };
-        let res = await axios(`http://localhost:3000/task/${e.target.id.value}`, options);
+        let res = await axios(
+          `http://localhost:3000/task/${e.target.id.value}`,
+          options
+        );
         let json = await res.data;
 
         location.reload();
       } catch (err) {
         let message = err.statusText || "Ocurrió un error";
-        $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+        $form.insertAdjacentHTML(
+          "afterend",
+          `<p><b>Error ${err.status}: ${message}</b></p>`
+        );
       }
     }
   }
 });
 
-d.addEventListener("click", async e => {
+d.addEventListener("click", async (e) => {
   if (e.target.matches(".edit")) {
     $title.textContent = "Editar Tarea";
     $form.nameData.value = e.target.dataset.nameData;
@@ -118,7 +129,9 @@ d.addEventListener("click", async e => {
   }
 
   if (e.target.matches(".delete")) {
-    let isDelete = confirm(`¿Estás seguro de que quieres eliminar la tarea ${e.target.dataset.id}?`);
+    let isDelete = confirm(
+      `¿Estás seguro de que quieres eliminar la tarea ${e.target.dataset.id}?`
+    );
 
     if (isDelete) {
       // Delete - DELETE
@@ -127,10 +140,13 @@ d.addEventListener("click", async e => {
         let options = {
           method: "DELETE",
           headers: {
-            "Content-type": "application/json; charset=utf-8"
-          }
+            "Content-type": "application/json; charset=utf-8",
+          },
         };
-        let res = await axios(`http://localhost:3000/task/${e.target.dataset.id}`, options);
+        let res = await axios(
+          `http://localhost:3000/task/${e.target.dataset.id}`,
+          options
+        );
         let json = await res.data;
 
         location.reload();
@@ -147,13 +163,13 @@ d.addEventListener("click", async e => {
 
     try {
       let options = {
-        method: "PATCH", 
+        method: "PATCH",
         headers: {
-          "Content-type": "application/json; charset=utf-8"
+          "Content-type": "application/json; charset=utf-8",
         },
         data: JSON.stringify({
-          taskDone: isChecked
-        })
+          taskDone: isChecked,
+        }),
       };
       let res = await axios(`http://localhost:3000/task/${taskId}`, options);
       let json = await res.data;
@@ -166,9 +182,3 @@ d.addEventListener("click", async e => {
     }
   }
 });
-
-
-
-
-
-
